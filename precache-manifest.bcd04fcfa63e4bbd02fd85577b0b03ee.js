bb04d7b23e82cef5a8257b665cc51a62d0253c98
Henry Adeyemi <youngach@gmail.com>
2020-03-11 02:14:30 +0100

    Updates

---
+++ precache-manifest.bcd04fcfa63e4bbd02fd85577b0b03ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4517b0a6adea8da4e0f3c3806c2d76",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ba17df44d64ee6f7bcbe",
+    "url": "/monsters-rolodex/static/css/main.983a52df.chunk.css"
+  },
+  {
+    "revision": "8dec1771fe1103c11c24",
+    "url": "/monsters-rolodex/static/js/2.892a4f54.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.892a4f54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba17df44d64ee6f7bcbe",
+    "url": "/monsters-rolodex/static/js/main.2830c2cb.chunk.js"
+  },
+  {
+    "revision": "dc3c3f2bba948092f93a",
+    "url": "/monsters-rolodex/static/js/runtime-main.1925a9c3.js"
+  }
+]);
